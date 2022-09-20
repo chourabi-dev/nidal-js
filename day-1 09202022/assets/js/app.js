d59f9c356e37c058ecbc0d9fde2bff2d76fecabc
chourabi-dev <chourabitahers62@gmail.com>
2022-09-20 20:41:39 +0100

    latest

---
+++ day-1 09202022/assets/js/app.js
@@ -0,0 +1,120 @@
+var countries = [
+    { id: 0 , name:"Tunisia" },
+    { id: 1 , name:"France" },
+    { id: 2 , name:"Germany" },
+    { id: 3 , name:"USA" },
+    { id: 4 , name:"Canada" }
+    
+]
+
+var vols = [
+    { name:"Tunisair" , prix:450, start: 0, destination: 1 },
+    { name:"Tunisair" , prix:520, start: 1, destination: 0 },
+    { name:"Turkish" ,  prix:1200, start: 1, destination: 2 },
+    { name:"Air dubai" , prix:950, start: 1, destination: 4 },
+    { name:"Air france" , prix:350, start: 4, destination: 3 },
+    
+]
+
+
+var searchByKeywordsElement = document.getElementById('key-search');
+var countriesElement = document.getElementById('countries');
+var priceElement = document.getElementById('price-range');
+var selectedPriceElement = document.getElementById('selected-price');
+var dataElement = document.getElementById('data');
+
+
+searchByKeywordsElement.addEventListener('keyup',function(event){
+    const value = event.target.value;
+
+    console.log(value);
+})
+
+
+countriesElement.addEventListener('change',function(event){
+    const value = event.target.value;
+
+    console.log(value);
+})
+
+
+
+priceElement.addEventListener('change',function(event){
+    const value = Number(event.target.value);
+
+
+    console.log(value);
+
+    let max = 0;
+
+    vols.map( (v)=>{  if( v.prix > max ) { max = v.prix } } );
+
+    // 100  max
+    // val  ? 
+
+    let res = (  (value * max)  /100 );
+
+    console.log(res);
+
+    selectedPriceElement.innerHTML=res+'$';
+})
+
+
+function initDATA(){
+    let blocHTML='';
+
+    countries.map( ( country )=>{
+        blocHTML = blocHTML + `<option value="${ country.id }">${country.name}</option>`;
+    })
+
+    countriesElement.innerHTML = blocHTML;
+
+}
+
+
+
+
+initDATA();
+
+
+function getCountryByID(id){
+    let country = null;
+
+
+    country = countries.filter( (c)=> c.id == id )[0].name;
+
+    return country;
+
+}
+
+
+function  showDATA(){
+
+    let blocHTML = '';
+
+    vols.map( (vol)=>{
+        blocHTML = blocHTML + `
+        <div class="row mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3>${vol.name}</h3>
+                            <p>
+                                <strong>${ getCountryByID(vol.start) }</strong> -  <strong>${ getCountryByID(vol.destination) }</strong>
+                            </p>
+
+                            <h3 class="text-success">${vol.prix}$</h3>
+                        </div>
+                    </div>
+                </div>
+                `;
+    })
+
+
+    dataElement.innerHTML = blocHTML;
+
+
+
+} 
+
+
+showDATA();
